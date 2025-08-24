42f4676d01c7293493887f4c918aa305162388df
Suman <sumanchakra2004@gmail.com>
2025-08-24 17:42:31 +0530

    added user and video model

---
+++ src/models/user.model.js
@@ -0,0 +1,98 @@
+import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+const userSchema=new Schema(
+{
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true,
+        index:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true,
+        
+    },
+    fullName:{
+        type:String,
+        required:true,
+        trim:true,
+        index:true
+    }, 
+    avatar:{
+        type:String,
+        required:true,
+    },
+    coverImage:{
+        type:String
+    },
+    watchHistory:[
+        {
+            type:Schema.Types.ObjectId,
+            ref:"Video"
+        }
+    ],
+    password:{
+        type:String,
+        required:[true,"Password is required"]
+    },
+    refreshToken:{
+        types:String
+    }
+
+
+},{timestamps:true})
+
+
+//Normal function binds this to the object.Arrow function does not bind this — it inherits it from outside.
+//so we dont use arrow function here
+userSchema.pre("save",async function(next) {
+    //we will only change pass when its modified,not always whenever user changes any other fields.
+    if(!this.isModified("password")) return next()
+
+    this.password=bcrypt.hash(this.password,10)// The number represents how much round password is to be hashed 
+    next()
+})
+
+//now we will check that entered password and saved passwords are same or not
+userSchema.methods.isPasswordCorrect=async function (password) {
+    return await bcrypt.compare(password,this.password)
+}
+
+//generate access token
+userSchema.methods.generateAccessToken=function(){
+    return jwt.sign(
+        {
+            _id:this._id,
+            username:this.username,
+            fullName:this.fullName,
+            email:this.email
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+        }
+)
+}
+//generate refresh token
+userSchema.methods.generateAccessToken=function(){
+    return jwt.sign(
+        {
+            _id:this._id,
+           
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+        }
+)
+}
+
+export const User = mongoose.model("User",userSchema)
