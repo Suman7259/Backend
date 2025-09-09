db5ae55421a4d404747633a033ef8f226d00cb09
Suman <sumanchakra2004@gmail.com>
2025-09-09 20:49:20 +0530

    login and loggedout user

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -82,7 +82,7 @@ userSchema.methods.generateAccessToken = function(){
 )
 }
 //generate refresh token
-userSchema.methods.generateAccessToken=function(){
+userSchema.methods.generateRefreshToken=function(){
     return jwt.sign(
         {
             _id:this._id,
