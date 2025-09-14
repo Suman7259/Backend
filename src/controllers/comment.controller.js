5aaeba592c7af9b62f12896e23cf95c59805826b
Suman <sumanchakra2004@gmail.com>
2025-09-15 03:05:00 +0530

    Enhanced comment functionality with error handling for add, update, and delete operations

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -9,18 +9,86 @@ const getVideoComments = asyncHandler(async (req, res) => {
     const {videoId} = req.params
     const {page = 1, limit = 10} = req.query
 
+    if(!videoId){
+        throw new ApiError(401,"Video not available")
+    }
 })
 
 const addComment = asyncHandler(async (req, res) => {
     // TODO: add a comment to a video
+    const {videoId} = req.params
+    const {content} = req.body
+
+    if(!videoId){
+        throw new ApiError(401,"Video not available")
+    }
+    if(!content || content.trim()===""){
+        throw new ApiError(401,"comment not available")
+    }
+
+    const comment=await Comment.create({
+        content,
+        video:videoId,
+        owner:req.user?._id
+    })
+
+    return res.status(200)
+        .json(new ApiResponse(200),
+        comment,
+        "comment added successfully")
 })
 
 const updateComment = asyncHandler(async (req, res) => {
     // TODO: update a comment
+    const {commentId} = req.params
+    const {content} = req.body
+
+    if(!commentId){
+        throw new ApiError(404,"comment not found")
+    }
+    if(!content || content.trim()===""){
+        throw new ApiError(401,"comment cannot be updated")
+    }
+
+    const comment = await Comment.findOneAndUpdate({
+        _id:commentId,
+        owner:req.user?._id,    
+    },
+    {$set:{content}},
+    {
+        new:true
+    })
+
+    if (!comment) {
+        throw new ApiError(404, "Comment not found or not authorized");
+    }
+
+    return res.status(200)
+    .json(new ApiResponse(200),comment,"comment updated succesfully")
+
+
 })
 
 const deleteComment = asyncHandler(async (req, res) => {
     // TODO: delete a comment
+    const {commentId}=req.params
+
+    if(!commentId){
+        throw new ApiError(404,"comment not found")
+    }
+
+    const comment=await Comment.findOneAndDelete({
+        _id:commentId,
+        owner:req.user?._id
+    })
+
+     if (!comment) {
+        throw new ApiError(404, "Comment not found or not authorized");
+    }
+
+    return res.status(200)
+    .json(new ApiResponse(200,comment,"comment deleted successfully"))
+
 })
 
 export {
